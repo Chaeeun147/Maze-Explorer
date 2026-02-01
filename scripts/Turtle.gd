098111f6b9ffdb0e4f94878ea5d14beaf49946d8
Chaeeun147 <chaeeunlim.love@gmail.com>
2026-02-01 00:31:19 -0500

    Collectable Item
    I added a collectable item in my game

--- scripts/Turtle.gd
+++ scripts/Turtle.gd
@@ -1,10 +1,6 @@
 extends Area2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-signal player_died
-const SPEED = 100.0
-var direction = 1.0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,16 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += direction * SPEED * delta
-
-
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
-	direction *= -1
-	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
-
+	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		emit_signal("player_died", body)
+	animated_sprite_2d.animation = "collected"
